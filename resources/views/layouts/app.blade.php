6ef8417982a17f63f4505178032f6cb8602c4312
sergio vieira <sergiosvieira@gmail.com>
2020-02-05 15:19:37 -0300

    Melhorias na interface do navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'IFCE - Controle de Chaves') }}</title>
 
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
     <!-- Scripts -->
@@ -29,71 +29,41 @@
     @guest
     @else
     <nav class="navbar navbar-expand-lg navbar-dark bg-success">
-        <a class="navbar-brand" href="#">IFCE - Controle de Chaves</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Opções">
+        <a class="navbar-brand" href="#">
+            <img src="{{ asset('img/ifce-brand.png')}}" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <a class="btn btn-info" style="margin-right: 5px;" href="{!! url('/sectors'); !!}" role="button">Setores</a>
-        <a class="btn btn-info" style="margin-right: 5px;" href="{!! url('/subsectors'); !!}"
-            role="button">Subsetores</a>
-        <a class="btn btn-info" style="margin-right: 5px;" href="{!! url('/keys'); !!}" role="button">Chaves</a>
-        <a class="btn btn-info" style="margin-right: 5px;" href="{!! url('/transactions'); !!}"
-            role="button">Entregar/Devolver
-            Chaves</a>
-            <a class="btn btn-info" style="margin-right: 5px;" href="{!! url('/history'); !!}"
-            role="button">Histórico das Chaves</a>
-        {{-- <a class="btn btn-info" style="margin-right: 5px;" href="{!! url('/keys'); !!}" role="button">Chaves</a> --}}
-        {{-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="{!! url('/sectors'); !!}" id="sectorDropdown"
-                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Setores
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="userDropdown">
-                        <a class="dropdown-item" href="#">Listar</a>
-                        <a class="dropdown-item" href="#">Cadastrar</a>
-                    </div>
-                </li> <!-- Users DropDown -->
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
-                        Usuários
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="userDropdown">
-                        <a class="dropdown-item" href="#">Listar</a>
-                        <a class="dropdown-item" href="#">Cadastrar</a>
-                    </div>
-                </li> <!-- Users DropDown -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="keysDropdown" role="button" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
-                        Chaves
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="keysDropdown">
-                        <a class="dropdown-item" href="#">Listar</a>
-                        <a class="dropdown-item" href="#">Cadastrar</a>
-                    </div>
-                </li> <!-- Users DropDown -->
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Retirar Chaves</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Devolver Chves</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown"
-                        aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-user"></i> {{ Auth::user()->name }} </a>
-        <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
-            <a class="dropdown-item" href="#">Minha Conta</a>
-            <a class="dropdown-item" href="#">Sair</a>
-        </div>
-        </li>
-        </ul>
-        </div> --}}
+              <li class="nav-item active">
+                <a class="nav-link" href="{!! url('/sectors'); !!}" role="button">Setores</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="{!! url('/subsectors'); !!}" role="button">Subsetores</a>
+              </li>
+              <li class="nav-item active">
+                  <a href="{!! url('/keys'); !!}" class="nav-link">Chaves</a>
+              </li>
+              <li class="nav-item active">
+                <a href="{!! url('/transactions'); !!}" class="nav-link">Entregar / Devolver Chaves</a>
+              </li>
+              <li class="nav-item active">
+                <a href="{!! url('/history'); !!}" class="nav-link">Histórico</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-user"></i> {{ Auth::user()->name }} 
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
+                    <a class="dropdown-item" href="#">Minha Conta</a>
+                    <a class="dropdown-item" href="#">Sair</a>
+                </div>
+              </li>                  
+            </ul>
+          </div>
     </nav> <!-- Navigator Bar -->
     @endguest
     <!-- <div class="container"> -->
